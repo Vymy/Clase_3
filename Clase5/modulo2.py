ae751bc9c60fae62cc087b499283fd99a8dcd236
Vymy <vymypr0jet@gmail.com>
2022-05-16 20:50:59 -0500

    Fallo

---
+++ Clase5/modulo2.py
@@ -0,0 +1,37 @@
+# pasar todo lo del monitor 1 a un archivo de texto
+# crear una carpeta donde se almacenaran los logs 
+# crear un archivo con X nombre 
+# insertar la informacion recabada en el archivo
+import psutil
+import os
+
+# nucles del cpu
+cpu_nucleos = psutil.cpu_count()
+cpu_frecuencia = psutil.cpu_freq()
+
+# memoria
+memoria_virtual = psutil.virtual_memory()
+
+# disco
+disco_uso = psutil.disk_usage('/')
+
+os.system('clear')
+
+info = "Informacion del sistema"
+nuc = "Nucleos del CPU => ", str(cpu_nucleos)
+cpu = "Frecuenca del CPU => ", str(cpu_frecuencia)
+mem ="Memoria virutal => ", str(memoria_virtual)
+disk = "Uso de disco => ", str(disco_uso)
+
+print(info)
+print(nuc)
+print(cpu)
+print(mem)
+print(disk)
+
+with open("text2.txt", "w") as archivo:
+    archivo.write(info)
+    archivo.write(nuc)
+    archivo.write(cpu)
+    archivo.write(mem)
+    archivo.write(disk)
